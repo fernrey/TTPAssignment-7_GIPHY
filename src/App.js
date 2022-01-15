d5807e70e3565a40ff7c0a0d1ca21261ca9f5bd2
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-15 00:45:03 -0500

    Fixed the bug on displaying the gif when clicked the random search button

--- src/App.js
+++ src/App.js
@@ -1,70 +1,50 @@
 import './index.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Search from './Components/Search.js'
 import Cards from './Components/Cards.js'
-import api from "./api"
 
 export default function App() {
-const [gifsHolder, setGifs] = useState([]);
-const API_KEY = api();
+    const [gifsHolder, setGifs] = useState([]);
+    const [random, setRandom] = useState("")
 
-const fetchApi= () => {
- let url = `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
-        fetch(url)
-        .then(response => response.json()).then((gifs) => {
-            // console.log(gifs);
-            
-            setGifs(gifs['data'])
-            
-            console.log(`only array so we can map`)
-            
-            console.log(gifs['data'])
-            
+    // getData() returns from Search.js
+    let  getData = (isRandom, arryHoldResultSearch) =>{
+        console.log(isRandom)
+        setRandom(isRandom)
+        if (isRandom === true) {
+            //console.log(arryHoldResultSearch)
+            setGifs(arryHoldResultSearch)
+        } else {
+            //console.log(arryHoldResultSearch['data'])
+            setGifs(arryHoldResultSearch['data'])
+        }
+    }
+    //console.log(random)
+    //console.log(gifsHolder)
+
+    const display = () => {
+        if(!random) {
+            return gifsHolder.map((el) => (
+                <Cards key={el.id} src = {el.images.fixed_height.url} />
+              ))
+        } else {
+            return <Cards src = {gifsHolder} />
+        }
+    }
     
-        }).catch((error) => {
-    
-            console.log(error.message)
-        })
-       
-  }
-
-   let  getData = (term, arryHoldResultSearch) =>{
-   console.log("From app here is the term" + term )
-   console.log(arryHoldResultSearch['data'])
-   setGifs(arryHoldResultSearch['data'])
-
-	}
-
-useEffect(() => {
-  fetchApi()
- 
-}, [])
-  console.log(gifsHolder)
-  return (
+    return (
       <div>
-
-    <nav> 
-   
-        <div className ="logo"> <h2>GIF Search Generator</h2></div>
-        <ul className="bar">
-        </ul>
-    </nav> 
-   
-      {/*<div className="searchBar">
-      <form>
-      <h1>Enter A Word </h1>
-      <input className="input" type="text" id="searchButtonText" ></input>
-      <button className="submit" id="searchButton" >Search</button>
-      <button className="submit" id="TrendingButton">Trending</button>
-      <button className="submit" id="randomButton">Random GIF</button>
-      </form>
-    </div>*/}
-     <Search getData={getData} />
-
-      {gifsHolder.map((el) => (
-          <Cards key={el.id} src = {el.images.fixed_height.url} />
-        ))}
-    </div> 
+          <nav> 
+              <div className ="logo">
+                   <h2>GIF Search Generator</h2>
+              </div>
+              <ul className="bar"></ul>
+            </nav> 
+            
+            <Search getData={getData} />
+            
+            {display()}
+        </div> 
   );
 }
 
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,69 +1,79 @@
-import React, { useEffect, useState } from "react";
-import Cards from "./Cards";
-import Loader from "./Loader";
-import api from "../api"
+import React, { useState } from "react";
+//import api from "../api"
 import '../index.css';
 
 export default function Search ({getData}){
-const [term, setTerm] = useState("");
-const [gifsHolder, setGifsHolder] = useState("");
-//const API = api();
-const API_KEY = api();
-const userInput = "dogs";
-const [cards, setCards] = useState([]);
-const [isLoading, setIsloading] = useState(false);
+    
+    const [term, setTerm] = useState("");
+    const [isRandom, setIsRandom] = useState(false)
+    const [searchType, setSearchType] = useState("")
+    const API_KEY = process.env.REACT_APP_GIPHY_KEY
+    //const [gifsHolder, setGifsHolder] = useState("");
+    //const API = api();
+    //const userInput = "dogs";
+    //const [cards, setCards] = useState([]);
+    //const [isLoading, setIsloading] = useState(false);
 
-const [searchType, setSearchType] = useState("")
-
-const fetchData = (term) => {
-	setIsloading(true);
+    
+    const fetchData = (term) => {
+	//setIsloading(true);
 	const url = `http://api.giphy.com/v1/gifs/${searchType}?q=${term}&api_key=${API_KEY}&limit=40`;
+    console.log("URL: " + url)
     fetch(url)
       .then(response => {
         return response.json() 
       })
       .then(data => {
-        setCards(data.data)
-        console.log(data)
-        setIsloading(false)
-        setGifsHolder(data)
-        getData(url, data)
+          //console.log(searchType)
+          if (searchType === 'random') {
+              setIsRandom(true)
+              console.log("Test: " + data.data.images.fixed_height.url)
+              //setCards(data.data.images.fixed_height.url)
+              //setGifsHolder(data.data.images.fixed_height.url)
+              console.log(isRandom)
+              console.log("URL: " + url)
+              getData(!isRandom, data.data.images.fixed_height.url)
+              
+          }else {
+           // setCards(data.data)
+           // console.log(data)
+            //setIsloading(false)
+           // setGifsHolder(data)
+           console.log("URL: " +url)
+           getData(isRandom, data)
         
-      })
-  }
+          }
+        })
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
         fetchData(term);
         console.log("handleSubmit")
 }
-const handChange = (e) => {
-  	console.log(e.target.value)
-  	setTerm(e.target.value);
-  }
+    const handChange = (e) => {
+        //console.log(e.target.value)
+  	    setTerm(e.target.value);
+    }
 
-
- // useEffect(() => {
- // 	fetchData()
- //    }, []);
-return (
-	<div>
-   <div className="searchBar" >
-    <form onSubmit={handleSubmit}>
-        <input  
-            type="text" 
-            name="GIF"
-            placeholder="Enter GIF"
-            className="input"
-            onChange={handChange}/>
-        <button className="submit" onClick = {() => setSearchType("search")}>Search</button>
-        <button className="submit" onClick = {() => setSearchType("trending")}>Trending</button>
-        <button className="submit" onClick = {() => setSearchType("random")}>Randome GIF</button>
-    </form>
-    </div>
-    </div>
-    )
-	
-}
+    return (
+	    <div>
+            <div className="searchBar" >
+                <form onSubmit={handleSubmit}>
+                    <input  
+                        type="text" 
+                        name="GIF"
+                        placeholder="Enter GIF"
+                        className="input"
+                        onChange={handChange}
+                    />
+                    <button className="submit" onClick = {() => setSearchType("search")}>Search</button>
+                    <button className="submit" onClick = {() => setSearchType("trending")}>Trending</button>
+                    <button className="submit" onClick = {() => setSearchType("random")}>Randome GIF</button>
+                </form>
+            </div>
+        </div>
+        )
+    }
 
 
 
